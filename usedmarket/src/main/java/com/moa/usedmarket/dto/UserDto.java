7af20c1f761ef7c3f176385a592d121c841bced7
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2024-01-27 16:30:11 +0900

    Delete usedmarket directory

--- usedmarket/src/main/java/com/moa/usedmarket/dto/UserDto.java
+++
@@ -1,9 +0,0 @@
-package com.moa.usedmarket.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDto {
-    private String username;
-    private String password;
-}
--- usedmarket/src/main/java/com/moa/usedmarket/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moa.usedmarket.repository;
-
-import com.moa.usedmarket.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    boolean existsByUsername(String username);
-    UserEntity findByUsernameAndPassword(String username, String password);
-}
